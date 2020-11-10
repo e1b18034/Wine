bf303da6dfd11afe9e0ec04cfa427954d599a4b1
e1b18034 <e1b18034@oit.ac.jp>
2020-11-10 15:32:04 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/beef_good/wine/WineApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.beef_good.wine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
