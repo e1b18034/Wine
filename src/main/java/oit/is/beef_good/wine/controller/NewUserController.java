b980f3ce42ba4571b0fb00bf28b49d8f4736cc30
tantaka0303 <hanshintigers.liked.2038@gmail.com>
2020-11-13 11:55:02 +0900

    UserMapperとNewUserControllerの編集

--- src/main/java/oit/is/beef_good/wine/controller/NewUserController.java
+++ src/main/java/oit/is/beef_good/wine/controller/NewUserController.java
@@ -1,14 +1,53 @@
 package oit.is.beef_good.wine.controller;
 
+import javax.management.modelmbean.ModelMBeanOperationInfo;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import oit.is.beef_good.wine.model.User;
+import oit.is.beef_good.wine.model.UserMapper;
 
 @Controller
 @RequestMapping("/new_user")
 public class NewUserController {
+  @Autowired
+  private UserMapper userMapper;
+
   @GetMapping("/form")
   public String form() {
     return "new_user_form.html";
   }
+
+  @PostMapping("/regist")
+  public String regist(@RequestParam String user_id, @RequestParam String user_name, @RequestParam String user_pwd_1,
+      @RequestParam String user_pwd_2, ModelMap model) {
+    if (user_id.equals("") || user_name.equals("") || user_pwd_1.equals("") || user_pwd_2.equals("")) {
+      model.addAttribute("message", "全ての情報を入力してください");
+      return "new_user_form.html";
+    }
+    if (user_pwd_1.equals(user_pwd_2) == false) {
+      model.addAttribute("message", "パスワードが違います");
+      return "new_user_form.html";
+    }
+    int num_of_record = userMapper.isExist(user_id);
+    if (num_of_record != 0) {
+      model.addAttribute("message", "ユーザーIDが重複しています");
+      return "new_user_form.html";
+    }
+    User user = new User();
+    user.setUser_id(user_id);
+    user.setUser_name(user_name);
+    user.setCryptedUserPwd(user_pwd_1);
+
+    userMapper.insertUser(user);
+
+    return "user_registered.html";
+  }
+
 }
--- src/main/java/oit/is/beef_good/wine/model/UserMapper.java
+++ src/main/java/oit/is/beef_good/wine/model/UserMapper.java
@@ -4,9 +4,16 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Insert;
 
 @Mapper
-public interface UserMapper{
+public interface UserMapper {
   @Select("SELECT * from user")
   List<User> getAllUsers();
+
+  @Select("SELECT count(*) from user where user_id = #{user_id}")
+  int isExist(String user_id);
+
+  @Insert("INSERT INTO user (user_id,user_name,user_pwd) VALUES (#{user_id},#{user_name},#{user_pwd});")
+  int insertUser(User user);
 }
