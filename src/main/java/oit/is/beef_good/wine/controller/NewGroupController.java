6f115ba3ab810a96d89f03de8401cc5e8580d9cb
tantaka0303 <hanshintigers.liked.2038@gmail.com>
2020-11-17 16:31:03 +0900

    グループ作成機能の追加

--- src/main/java/oit/is/beef_good/wine/controller/NewGroupController.java
+++ src/main/java/oit/is/beef_good/wine/controller/NewGroupController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.beef_good.wine.model.Group;
 import oit.is.beef_good.wine.model.GroupMapper;
@@ -18,11 +20,32 @@ public class NewGroupController {
   private GroupMapper groupMapper;
 
   @GetMapping("")
-  public String page(ModelMap model) {
-    List<Group> groups = groupMapper.getAllGroups();
-
-    model.addAttribute("groups", groups);
-
+  public String page() {
     return "new_group_page.html";
+  }
+
+  @PostMapping("/regist")
+  public String regist(@RequestParam String group_id, @RequestParam String group_name, @RequestParam String group_pwd_1,
+      @RequestParam String group_pwd_2, ModelMap model) {
+    if (group_id.equals("") || group_name.equals("") || group_pwd_1.equals("") || group_pwd_2.equals("")) {
+      model.addAttribute("message", "全ての情報を入力してください");
+      return "new_group_page.html";
+    }
+    if (group_pwd_1.equals(group_pwd_2) == false) {
+      model.addAttribute("message", "パスワードが一致しません");
+      return "new_group_page.html";
+    }
+
+    if (groupMapper.isExist(group_id) != 0) {
+      model.addAttribute("message", "グループIDが重複しています");
+      return "new_group_page.html";
+    }
+    Group group = new Group();
+    group.setGroup_id(group_id);
+    group.setGroup_name(group_name);
+    group.setGroup_pwd(group_pwd_1);
+
+    groupMapper.insertGroup(group);
+    return "group_registered.html";
   }
 }
--- src/main/java/oit/is/beef_good/wine/model/GroupMapper.java
+++ src/main/java/oit/is/beef_good/wine/model/GroupMapper.java
@@ -2,6 +2,7 @@ package oit.is.beef_good.wine.model;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -9,4 +10,11 @@ import org.apache.ibatis.annotations.Select;
 public interface GroupMapper {
   @Select("SELECT * FROM `group`")
   List<Group> getAllGroups();
+
+  @Select("SELECT count(*) FROM `group` WHERE group_id = #{group_id}")
+  int isExist(String group_id);
+
+  @Insert("INSERT INTO `group` (group_id,group_name,group_pwd) VALUES (#{group_id},#{group_name},#{group_pwd})")
+  int insertGroup(Group group);
+
 }
