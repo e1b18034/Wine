9be5735974edca9aa7b1bd01ce1880b10f8ce8e0
e1b18034 <e1b18034@oit.ac.jp>
2020-12-01 15:59:50 +0900

    オリジナルのログインフォームを使用してのDBとの連携

--- src/main/java/oit/is/beef_good/wine/security/WineAuthConfiguration.java
+++ src/main/java/oit/is/beef_good/wine/security/WineAuthConfiguration.java
@@ -26,11 +26,7 @@ public class WineAuthConfiguration extends WebSecurityConfigurerAdapter {
    */
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    // user1 pwd11101624
-    List<User> users = this.userMapper.getAllUsers();
-    for (User user : users) {
-      auth.inMemoryAuthentication().withUser(user.getUser_id()).password(user.getUser_pwd()).roles("USER");
-    }
+    //
   }
 
   @Bean
@@ -43,28 +39,9 @@ public class WineAuthConfiguration extends WebSecurityConfigurerAdapter {
    */
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-
-    // Spring Securityのフォームを利用してログインを行う
-    http.formLogin();
-
-    // http://localhost:8000/sample3 で始まるURLへのアクセスはログインが必要
-    // antMatchers().authenticated がantMatchersへのアクセスに認証を行うことを示す
-    // antMatchers()の他にanyRequest()と書くとあらゆるアクセス先を表現できる
-    // authenticated()の代わりにpermitAll()と書くと認証処理が不要であることを示す
-    // http.authorizeRequests().antMatchers("/chat_page/**").authenticated();
-    // http.authorizeRequests().antMatchers("/new_user/**").authenticated();
-    // http.authorizeRequests().antMatchers("/new_group/**").authenticated();
-    // http.authorizeRequests().antMatchers("/entry_group/**").authenticated();
     http.authorizeRequests().anyRequest().permitAll();
-
-    // Spring Securityの機能を利用してログアウト．ログアウト時は http://localhost:8000/ に戻る
     http.logout().logoutSuccessUrl("/");
 
-    /**
-     * 以下2行はh2-consoleを利用するための設定なので，開発が完了したらコメントアウトすることが望ましい
-     * CSRFがONになっているとフォームが対応していないためアクセスできない
-     * HTTPヘッダのX-Frame-OptionsがDENYになるとiframeでlocalhostでのアプリが使えなくなるので，H2DBのWebクライアントのためだけにdisableにする必要がある
-     */
     http.csrf().disable();
     http.headers().frameOptions().disable();
 
