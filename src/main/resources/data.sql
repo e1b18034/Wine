295f576c398c913138746e7e01e4c2e4d7e1d6a7
e1b18034 <e1b18034@oit.ac.jp>
2020-12-08 18:02:59 +0900

    フレンド登録リクエストの実装

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,3 @@
 INSERT INTO user (user_id,user_name,user_pwd) VALUES ('user1','user1','$2y$10$lU.sJhPia8UQtiigHIMhEOqYYsKDgMe0wxJxuEMAO2MpMGKsqMs3O');
 INSERT INTO `group` (group_id, group_name, group_pwd) VALUES ('group1', 'BeefとWine', '$2y$10$hR9Qs6kwslHhVF4ON87fOO7voVFXQgaafLIjcuJgm3AdUUIdQaCIy');
 INSERT INTO user (user_id, user_name, user_pwd) VALUES ('user2', 'wine', '$2a$10$mDGdD0CJmhv3CNu3rWc68OlPKQXGmcb73U9jzSTo.EHtHKqOjDuru');
-INSERT INTO friend (user_id,friend_id,status) VALUES ('user1','user2',TRUE);
