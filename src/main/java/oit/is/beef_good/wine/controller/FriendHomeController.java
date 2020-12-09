1a0462887a076445b769e0c71a25620397f04fb2
tantaka0303 <hanshintigers.liked.2038@gmail.com>
2020-12-09 12:54:14 +0900

    フレンド一覧の表示

--- src/main/java/oit/is/beef_good/wine/controller/FriendHomeController.java
+++ src/main/java/oit/is/beef_good/wine/controller/FriendHomeController.java
@@ -1,14 +1,25 @@
 package oit.is.beef_good.wine.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import oit.is.beef_good.wine.model.FriendMapper;
 
 @Controller
 @RequestMapping("/friend_home")
 public class FriendHomeController {
+  @Autowired
+  private FriendMapper friendMapper;
   @GetMapping("")
-  public String page() {
+  public String page(ModelMap model) {
+    List<String> friendList = this.friendMapper.getFriendListById("user2");
+    model.addAttribute("friend_list", friendList);
     return "friend_home.html";
   }
 }
