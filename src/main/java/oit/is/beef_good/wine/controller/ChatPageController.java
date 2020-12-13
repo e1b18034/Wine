68461bf209e21f77f2d15feb38b76187752be7bd
e1b18034 <e1b18034@oit.ac.jp>
2020-12-13 19:21:28 +0900

    グループチャット機能の実装

--- src/main/java/oit/is/beef_good/wine/controller/ChatPageController.java
+++ src/main/java/oit/is/beef_good/wine/controller/ChatPageController.java
@@ -1,8 +1,6 @@
 package oit.is.beef_good.wine.controller;
 
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import oit.is.beef_good.wine.model.BelongMapper;
-import oit.is.beef_good.wine.model.Chat;
 import oit.is.beef_good.wine.model.ChatData;
 import oit.is.beef_good.wine.model.FriendChatMapper;
 import oit.is.beef_good.wine.model.FriendMapper;
@@ -27,9 +24,6 @@ import oit.is.beef_good.wine.service.AsyncChat;
 @Controller
 @RequestMapping("/chat_page")
 public class ChatPageController {
-  @Autowired
-  private Chat chat;
-
   @Autowired
   private BelongMapper belongMapper;
 
@@ -45,19 +39,11 @@ public class ChatPageController {
   @Autowired
   private GroupChatMapper groupChatMapper;
 
-  private void commonProcess(ModelMap model, HttpSession session) {
-    String user_id = new WineAuthentication(session).getUserId();
-    List<String> groupList = belongMapper.getBelongingGroupId(user_id);
-    model.addAttribute("group_list", groupList);
-  }
-
   @GetMapping("")
   public String chatPage(ModelMap model, HttpSession session) {
     if (!new WineAuthentication(session).isAuthenticated()) {
       return WineAuthentication.authenticate("/chat_page");
     }
-
-    this.commonProcess(model, session);
 
     return "chat_page.html";
   }
@@ -65,7 +51,7 @@ public class ChatPageController {
   @GetMapping("/group_chat")
   public String groupChat(@RequestParam String receiver, ModelMap model, HttpSession session) {
     if (!new WineAuthentication(session).isAuthenticated()) {
-      return WineAuthentication.authenticate("/chat_page");
+      return WineAuthentication.authenticate("/chat_page/group_chat?receiver=" + receiver);
     }
 
     model.addAttribute("receiver", receiver);
@@ -91,39 +77,30 @@ public class ChatPageController {
     return emitter;
   }
 
-  @GetMapping("/async_update")
-  public SseEmitter asyncGroupChat(@RequestParam String group_id, HttpSession session) {
-    final SseEmitter emitter = new SseEmitter();
-
-    if (new WineAuthentication(session).isAuthenticated()) {
-      String user_id = new WineAuthentication(session).getUserId();
-      if (this.belongMapper.isExist(group_id, user_id) == 1) {
-        this.asyncChat.update(emitter, group_id);
-      } else {
-        this.asyncChat.error(emitter);
-      }
-    }
-
-    return emitter;
-  }
-
-  @PostMapping("/send")
-  public String sendMessage(@RequestParam String chat, @RequestParam String group_id, ModelMap model,
+  @PostMapping("/group_chat/send")
+  public String sendGroupChat(@RequestParam String chat_data, @RequestParam String receiver, ModelMap model,
       HttpSession session) {
-    if (!new WineAuthentication(session).isAuthenticated()) {
-      return WineAuthentication.authenticate("/chat_page");
+    WineAuthentication auth = new WineAuthentication(session);
+
+    if (!auth.isAuthenticated()) {
+      return WineAuthentication.authenticate("/chat_page/group_chat?receiver=" + receiver);
     }
 
-    String user_id = new WineAuthentication(session).getUserId();
-    if (this.belongMapper.isExist(group_id, user_id) == 1) {
-      this.chat.addChatData(user_id, chat, group_id);
-    } else {
-      model.addAttribute("message", "所属していないグループにメッセージを送信することはできません");
-    }
+    String user_id = auth.getUserId();
+    String date_time = LocalDateTime.now().toString();
+    int data_type = ChatData.TYPE_TEXT;
 
-    this.commonProcess(model, session);
+    ChatData chatData = new ChatData();
+    chatData.setSender(user_id);
+    chatData.setReceiver(receiver);
+    chatData.setDate_time(date_time);
+    chatData.setData_type(data_type);
+    chatData.setChat_data(chat_data);
 
-    model.addAttribute("group_id", group_id);
+    this.groupChatMapper.insertChatData(chatData);
+
+    model.addAttribute("receiver", receiver);
+    model.addAttribute("chat_type", "/group_chat");
 
     return "chat_page.html";
   }
@@ -163,7 +140,7 @@ public class ChatPageController {
     WineAuthentication auth = new WineAuthentication(session);
 
     if (!auth.isAuthenticated()) {
-      return WineAuthentication.authenticate("/friend_chat?receiver=" + receiver);
+      return WineAuthentication.authenticate("/chat_page/friend_chat?receiver=" + receiver);
     }
 
     String user_id = auth.getUserId();
