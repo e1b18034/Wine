34819022f46f65b9ddd0bc894728d7d702fc5a8e
e1b18062 <e1b18062@st.oit.ac.jp>
2021-01-05 16:46:28 +0900

    データベースの永続化

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,29 +1,29 @@
-CREATE TABLE user(
+CREATE TABLE IF NOT EXISTS user(
   user_id CHAR PRIMARY KEY,
   user_name CHAR NOT NULL,
   user_pwd TEXT NOT NULL
 );
 
-CREATE TABLE `group`(
+CREATE TABLE IF NOT EXISTS `group`(
   group_id CHAR PRIMARY KEY,
   group_name CHAR NOT NULL,
   group_pwd TEXT NOT NULL
 );
 
-CREATE TABLE belong (
+CREATE TABLE IF NOT EXISTS belong (
   group_id CHAR REFERENCES `group` (group_id),
   user_id CHAR REFERENCES user (user_id),
   PRIMARY KEY (group_id, user_id)
 );
 
-CREATE TABLE friend (
+CREATE TABLE IF NOT EXISTS friend (
   user_id CHAR REFERENCES user (user_id),
   friend_id CHAR REFERENCES user(user_id),
   status BOOLEAN NOT NULL,
   PRIMARY KEY (user_id,friend_id)
 );
 
-CREATE TABLE friend_chat (
+CREATE TABLE IF NOT EXISTS friend_chat (
   id INT IDENTITY(1,1),
   sender CHAR REFERENCES user (user_id),
   receiver CHAR REFERENCES user (user_id),
@@ -32,7 +32,7 @@ CREATE TABLE friend_chat (
   chat_data CHAR
 );
 
-CREATE TABLE group_chat (
+CREATE TABLE IF NOT EXISTS group_chat (
   id INT IDENTITY(1,1),
   sender CHAR REFERENCES user (user_id),
   receiver CHAR REFERENCES `group` (group_id),
